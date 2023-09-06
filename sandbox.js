eea4fd55372f6a8ab2b3fdc5228a6c193be1fc39
asaprosa <91055197+asaprosa@users.noreply.github.com>
2023-09-06 18:52:06 +0530

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,58 @@
+// Shery.mouseFollower();
+// Shery.makeMagnet('.magnet');
+
+// gsap.to(".leftelm", {
+//     scrollTrigger: {
+//       trigger: "#fimages",
+//       pin: true,
+//       start: "top top",
+//       end: "bottom bottom",
+//       endTrigger: ".last",
+//       scrub: 1,
+//     },
+//     y: "-300%",
+//     ease: Power1,
+//   });
+
+// const cursor = new MouseFollower({
+//     container: '.mf-container',
+//     speed: 0.3
+// });
+
+// const cursor = new MouseFollower();
+// const el = document.querySelector('.magnet');
+
+// el.addEventListener('mouseenter', () => {
+//     cursor.setSkewing(3);
+// });
+
+// el.addEventListener('mouseleave', () => {
+//     cursor.removeSkewing();
+// });
+// import Cursor from "cursor";
+// import Magnetic from "magnetic";
+
+// // Basic cursor
+// const cursor = new Cursor();
+
+// // Cursor options with defaults
+// const cursor = new Cursor({
+//     container: "body", // container to attach
+//     speed: 0.7, // default speed
+//     ease: "expo.out", // default ease (gsap)
+//     visibleTimeout: 300 // disappear timeout
+// });
+
+// // Basic magnetic
+// const magnetic = new Magnetic(el);
+
+// // Magnetic options with default
+// const magnetic = new Magnetic(el, {
+//     y: 0.2, // horizontal delta
+//     x: 0.2, // vertical delta
+//     s: 0.2, // speed
+//     rs: 0.7 // release speed
+// });
+
+// // Automatic handle magnetic elements through attribute
+// $('[data-magnetic]').each(function () {new Magnetic(this);});
